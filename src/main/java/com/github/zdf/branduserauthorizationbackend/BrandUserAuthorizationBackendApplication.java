4c23a7edf94c4ef4f5c7d769d80158bbf30c36a4
Francis-Zheng <824254922@qq.com>
2019-08-26 15:06:48 +0800

    Initial commit

---
+++ src/main/java/com/github/zdf/branduserauthorizationbackend/BrandUserAuthorizationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.zdf.branduserauthorizationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandUserAuthorizationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandUserAuthorizationBackendApplication.class, args);
+    }
+
+}
