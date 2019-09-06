0b8d2c4b37a13ba0a8d72560ffa37c464f2a9b17
Francis-Zheng <824254922@qq.com>
2019-09-06 15:37:02 +0800

    完善docker-compose部署方式，能够导入初始化数据到brand-report-backend的mongodb中

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-echo "正在启动品牌管理系统后端程序"
+echo "正在导入用户和角色数据..."
+
+mongoimport --host mongo:27017 --db brand_user_authorization --collection role --file /data/role.json
+mongoimport --host mongo:27017 --db brand_user_authorization --collection user --file /data/user.json
+
+echo "正在启动品牌管理系统后端程序..."
 java -jar /tmp/brand-user-authorization-backend-0.0.1-SNAPSHOT.jar
--- docker/beforestart.sh
+++ docker/beforestart.sh
@@ -7,3 +7,4 @@ cd ../
 echo "构建后台程序jar文件"
 mvn package spring-boot:repackage
 cp target/brand-user-authorization-backend-0.0.1-SNAPSHOT.jar docker/backend/
+cp -r data/ docker/backend/
