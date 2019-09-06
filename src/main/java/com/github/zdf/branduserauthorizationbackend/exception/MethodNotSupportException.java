937962af6e024e55dea33be50d4c869a1b111f17
Francis-Zheng <824254922@qq.com>
2019-09-06 16:38:25 +0800

    之前漏加入git的文件

---
+++ src/main/java/com/github/zdf/branduserauthorizationbackend/exception/MethodNotSupportException.java
@@ -0,0 +1,10 @@
+package com.github.zdf.branduserauthorizationbackend.exception;
+
+public class MethodNotSupportException extends RuntimeException {
+    public MethodNotSupportException() {
+    }
+
+    public MethodNotSupportException(String s) {
+        super(s);
+    }
+}
